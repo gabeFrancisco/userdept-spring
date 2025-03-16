79097dc62e1654992dc09352db0211aeb396ebe0
gabeFrancisco <gabegit2@gmail.com>
2025-03-15 23:26:18 -0300

    entities and jpa config

---
+++ src/main/java/com/ellyon/userdept/entities/Department.java
@@ -0,0 +1,30 @@
+package com.ellyon.userdept.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Department(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
